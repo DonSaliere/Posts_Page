155c8856b0bb1d1f96c9d23b78276d4f39166209
Andrey <s.adnrey-klg@yandex.ru>
2020-11-13 12:15:10 +0300

    кастомные события и иммутабильность

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,27 +7,78 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
 
-const App = () => {
+const AppBlock = styled.div`
+	margin: 0 auto;
+`
 
-	const data = [
-		{ label: 'Going to learn React', important: true, id: "qqq" },
-		{ label: 'That is good', important: false, id: "www" },
-		{ label: 'I need a break...', important: false, id: "eee" }
-	]
+const StyledAppBlock = styled(AppBlock)`	
+	max-width: 800px;
+`
 
-	return (
-		<div className="app">
-			<AppHeader />
-			<div className="search-panel d-flex">
-				<SearchPanel />
-				<PostStatusFilter />
-			</div>
-			<PostList posts={data} />
-			<PostAddForm />
-		</div>
+export default class App extends Component {
 
-	)
+	constructor(props) {
+		super();
+		this.state = {
+			data: [
+				{ label: 'Going to learn React', important: true, id: 1 },
+				{ label: 'That is good', important: false, id: 2 },
+				{ label: 'I need a break...', important: false, id: 3 }
+			]
+		};
+		this.deleteItem = this.deleteItem.bind(this);
+		this.addItem = this.addItem.bind(this);
+
+		this.maxId = 4;
+	}
+
+	deleteItem(id) {
+		console.log(id);
+		this.setState(({ data }) => {
+			const index = data.findIndex(elem => elem.id === id);
+
+			const before = data.slice(0, index);
+			const after = data.slice(index + 1);
+
+			const newArr = [...before, ...after];
+			return {
+				data: newArr
+			}
+		});
+	}
+
+	addItem(body) {
+		const newItem = {
+			label: body,
+			important: false,
+			id: this.maxId++
+		}
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem];
+			return {
+				data: newArr
+			}
+		})
+	}
+
+	render() {
+		return (
+			<StyledAppBlock>
+				<AppHeader />
+				<div className="search-panel d-flex">
+					<SearchPanel />
+					<PostStatusFilter />
+				</div>
+				<PostList
+					posts={this.state.data}
+					onDelete={this.deleteItem} />
+				<PostAddForm
+					onAdd={this.addItem} />
+			</StyledAppBlock >
+
+		)
+	}
 }
 
-export default App;
