ede4ad6f9cab19af6087509b1010df48e58763c1
Andrey <s.adnrey-klg@yandex.ru>
2020-11-16 00:01:36 +0300

    Работа с элементами на странице

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,7 +5,7 @@ import { ListGroup } from 'reactstrap';
 import './post-list.css'
 
 
-const PostList = ({ posts, onDelete }) => {
+const PostList = ({ posts, onDelete, onToggleImportant, onToggleLike }) => {
 
 	const elements = posts.map((item) => {
 		if (typeof (item) === 'object' && isEmpty(item)) {
@@ -14,7 +14,9 @@ const PostList = ({ posts, onDelete }) => {
 				<li key={id} className='list-group-item'>
 					<PostListItem
 						{...itemProps}
-						onDelete={() => onDelete(id)} />
+						onDelete={() => onDelete(id)} 
+						onToggleImportant={() => onToggleImportant(id)}
+						onToggleLike={() => onToggleLike(id)}/>
 				</li>
 			)
 		}
